f30b705990f3e9488ab25b920705666522b5947f
jarray-thamer <101900982+jarray-thamer@users.noreply.github.com>
2024-01-07 11:19:30 +0100

    renaming some variables

--- src/App.js
+++ src/App.js
@@ -2,57 +2,57 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  // let todosArray = ['hello','world'];
-
-  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState([]);
   const inputRef = useRef("");
 
   const handleAddTodo = () => {
     if (inputRef.current.value) {
       const text = inputRef.current?.value;
-      setTodos([...todos, { completed: false, text }]);
+      setTodo([...todo, { completed: false, text }]);
       inputRef.current.value = "";
-      console.log("hundler", todos);
+      console.log("handler", todo);
     }
   };
 
   const handleItemDone = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].completed = !newTodos[index].completed;
-    setTodos(newTodos);
+    const newTodo = [...todo];
+    newTodo[index].completed = !newTodo[index].completed;
+    setTodo(newTodo);
   };
 
   const handleDeleteItem = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
+    const newTodo = [...todo];
+    newTodo.splice(index, 1);
+    setTodo(newTodo);
+  };
 
   return (
     <div className="App">
-    <h2>To Do List</h2>
-    <div className="to-do-container">
-      <ul>
-        {todos.map(({ text, completed }, index) => {
-          return (
-            <div className="item">
-              <li
-                className={completed ? "done" : ""}
-                key={index}
-                onClick={() => handleItemDone(index)}
-              >
-                {text}
-              </li>
-              <span onClick={() => handleDeleteItem(index)} className="trash">❌</span>
-            </div>
-          );
-        })}
-      </ul>
-      <input ref={inputRef} placeholder="Enter item..." />
-      <button onClick={handleAddTodo}>Add</button>
+      <h2>To Do List</h2>
+      <div className="to-do-container">
+        <ul>
+          {todo.map(({ text, completed }, index) => {
+            return (
+              <div className="item">
+                <li
+                  className={completed ? "done" : ""}
+                  key={index}
+                  onClick={() => handleItemDone(index)}
+                >
+                  {text}
+                </li>
+                <span onClick={() => handleDeleteItem(index)} className="trash">
+                  ❌
+                </span>
+              </div>
+            );
+          })}
+        </ul>
+        <input ref={inputRef} placeholder="Enter item..." />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
